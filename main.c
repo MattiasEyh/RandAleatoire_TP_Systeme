117eae9057d949f7e53073c340b9930fe73171db
Mattias Eyherabide <mattias.eyherabide@protonmail.com>
2020-11-21 13:43:19 +0100

    Version finale.

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+/**
+ * \file equilibreMemPartagee.c
+ * \author Mattias Eyherabide
+ * \version 1.0
+ * \date 21 Novembre 2020
+ *
+ * Créer un tableau en mémoire partagée (IPC) et
+ * remplis chaques cases du nombre d'une valeure aléatoire et vérifie
+ * si le tableau est équilibré.
+ * Plusieurs processus feront le traitement. Le nombre de processus est
+ * défini en argument.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -11,19 +24,67 @@
 #include <sys/wait.h>
 
 #define RAND_MAX 3000
+#define NB_TOTAL 20000000
 
-int *tableauDeNombreAleatoires;
-int idTableauPartage;
 
-key_t cleTableau;
-int proj_id;
+int *tableauDeNombreAleatoires; /*!< Tableau crée en mémoire */
+int idTableauPartage; /*!< Identifiant du tableau */
 
-sem_t *sem;
-sem_t *sem2;
+key_t cleTableau; /*!< Clé unique de la zone mémoire à partagée*/
+int proj_id; /*!< Identifiant du projet afin de créer la clé */
+
+sem_t *sem; /*!< Sémaphore gérant l'entrée des processus */
+
+/*!
+ * \brief Equilibre du tableau
+ * Méthode permetant de vérifier si le tableau généré est équilibré.
+ * Pour qu'il soit équilibré, la valeur entre la valeure minimale et maximale
+ * doit être inférieure à 5%.
+ */
+void equilibre()
+{
+    /* Vérifie si le nombres d'éléments totaux est correct. */
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+    int valMin, valMax, valTotal = 0;
+    double ecart;
+
+    printf("\n\nVERIFICATIONS"
+           "\n---------------");
+
+    for (int i = 0; i < RAND_MAX; i++)
+        valTotal += tableauDeNombreAleatoires[i];
+
+    printf("\nNombre de valeurs totales : %d\nNombre de valeurs attendues : %d", valTotal, NB_TOTAL);
+
+    if(valTotal == NB_TOTAL ) printf("\nOK"); else printf("\nErreur lors du remplissage du tableau.");
+
+
+    /* Vérifie si le tableau est équibré    */
+    /* - - - - - - - - - - - - - - - - - - -*/
+    for(int i=0; i<RAND_MAX; i++)
+    {
+        if(tableauDeNombreAleatoires[i] > valMax) valMax = tableauDeNombreAleatoires[i];
+        if(tableauDeNombreAleatoires[i] < valMin) valMin = tableauDeNombreAleatoires[i];
+
+        if(i==0) valMin = valMax = tableauDeNombreAleatoires[i];
+    }
+
+    printf("\n\nValeur minimale : %d, Valeur maximale : %d\n", valMin, valMax);
+
+    ecart = (((double)valMax-(double)valMin)/valMin)*100;
+
+    printf("\nLe tableau est équilibré si la différence entre min et max est inférieure à 5%%.");
+    printf("\nPourcentage : %f%%", ecart);
+    if (ecart > 5) printf("\n\nTableau non équilibré");
+    else printf("\n\nTableau équilibré");
+}
+
 
 int main(int argc, char *argv[]) {
 
-    int nbProcessus = atoi(argv[1]);
+    int nbProcessus = atoi(argv[1]); /*!< Nombres de processus faisant le traitement. */
+
     /*----------------------------------------*/
     /*      Création d'une clef unique.       */
     /*----------------------------------------*/
@@ -39,6 +100,8 @@ int main(int argc, char *argv[]) {
 
     printf("Clef générée : 0x%04x\n", cleTableau);
 
+
+
     /*----------------------------------------*/
     /*   Création de la mémoire partagée.     */
     /*----------------------------------------*/
@@ -51,14 +114,13 @@ int main(int argc, char *argv[]) {
         if (errno == EEXIST){
             // Mémoire non crée car déjà existante donc ouverture de cette mémoire :
             idTableauPartage= shmget(cleTableau, sizeof(int)*RAND_MAX, IPC_CREAT | 0666);
-            printf("Ouverture d'une mémoire partagée : %d\n", idTableauPartage);
+            printf("Ouverture d'une mémoire partagée. Identifiant : %d\n\n", idTableauPartage);
         }
     }
     else
-        printf("Création d'une mémoire partagée : %d\n", idTableauPartage);
+        printf("Création d'une mémoire partagée. Identifiant : %d\n\n", idTableauPartage);
 
     // Si impossible de créer la mémoire :
-    // -1 = erreur
     if (idTableauPartage == -1) {
         printf("Erreur dans la création de la mémoire partagée.");
     }
@@ -77,10 +139,10 @@ int main(int argc, char *argv[]) {
                 printf("Accès refusé !\n");
                 break;
             case ENOMEM:
-                printf("Mémoire saturée !");
+                printf("Mémoire saturée !\n");
                 break;
             default:
-                printf("Erreur inconnue");
+                printf("Erreur inconnue.\n");
                 break;
         }
     }
@@ -94,43 +156,29 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));
 
     for (int i = 0; i < nbProcessus; i++) //Pour chaques processus
-    {
         if (!fork())
         {
             sem_wait(sem);
 
-            printf("Le fils rentre");
+            printf("Le fils rentre..\n");
 
-            for (int i = 0; i < 20000000 / nbProcessus; i++)
+            for (int i = 0; i < NB_TOTAL / nbProcessus; i++)
             {
                 int rnd = rand() % RAND_MAX;
                 tableauDeNombreAleatoires[rnd]++;
             }
             sleep(1);
+            printf("Tableau remplis par un processus\n\n");
 
             sem_post(sem);
 
             exit(1);
         }
 
-    }
-
     for (int i = 0; i < nbProcessus; i++)
         wait(NULL);
 
-    // Affichage
-    /*for (int i = 0; i < RAND_MAX; i++)
-        printf("\nVal %d : %d", i, tableauDeNombreAleatoires[i]);*/
-
-
-
-    int valTotal = 0;
-    for (int i = 0; i < RAND_MAX; i++)
-    {
-       valTotal += tableauDeNombreAleatoires[i];
-    }
-    printf("\n\nValeurs totales : %d ", valTotal);
-
+    equilibre();
 
 
     shmctl(idTableauPartage,IPC_RMID,NULL);
